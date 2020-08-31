4fbf0d93d56659890f852e595ca214220e63bda2
MJunaidAftab <50157884+MJunaidAftab@users.noreply.github.com>
2020-08-31 06:49:17 -0400

    Add files via upload

--- Chapter 10 - Elementary Data Structures/Section 10.2.py
+++ Chapter 10 - Elementary Data Structures/Section 10.2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Linked List
+# # Linked Lists
 # 
 # linked list is a data structure in which the objects are arranged in a linear order. Unlike an array, however, in which the linear order is determined by the array indices, the order in a linked list is determined by a pointer in each object.
 # 
@@ -43,7 +43,7 @@ class Node(object):
 
 # We first write an auxilliary class.
 
-# In[49]:
+# In[2]:
 
 
 class DoublyLinked_List(object):
@@ -55,10 +55,10 @@ class DoublyLinked_List(object):
         self.tailclass = None
         print(self.head, self.tail)
         
-        #Initially the doubly linked list is empty so contains no head and tail.
+        #Initially the doubly linked list is empty so it contains no head and tail.
         
     def empty(self):
-        Boolean = self.head
+        Boolean = self.headclass
         
         if Boolean == None:
             
@@ -74,7 +74,7 @@ class DoublyLinked_List(object):
         
         x = Node(x)
         
-        if self.head == None:
+        if self.headclass == None:
             
             self.head = x.getData()
             self.headclass = x
@@ -196,7 +196,7 @@ class DoublyLinked_List(object):
 
 # We first test the DOUBLY_LINKED_LIST class above by running some code to both generate and manipulate a doubly linked list.
 
-# In[50]:
+# In[3]:
 
 
 linkedlist = DoublyLinked_List()
@@ -241,10 +241,164 @@ print(a, b, c)
 
 # ### Time Complexity of the Functions:
 # 
-# The time complexity of the SEARCH function on a list of $n$ elements is $\Theta (n)$ as we run a linear search algorithm. The running time for SEARCH function on a list of $n$ elements is $O(1)$. The running time of the DELETE function is also $O(1)$ since we only perform a number of constant time operations.
+# The time complexity of the SEARCH function on a list of $n$ elements is $\Theta (n)$ as we run a linear search algorithm. The running time for INSERT function on a list of $n$ elements is $O(1)$. The running time of the DELETE function is $O(n)$. Here we assume that we have implement a binary search to see if there exists a node that has a key value that we would like to be deleted. Of course, if we're given the pointers of the node we wish to delete, then the time complexity of the algorithm would $O(1)$.
 
-# In[ ]:
+# ### 10.2.4
+# 
+# ### Singly Linked List
+
+# In[4]:
 
 
+#We use the same NODE class since all we have to do is that we don't have to initialize the PREV pointers.
 
 
+class Singly_Linked_List(object):
+    
+    def __init__(self):
+        self.head = None
+        self.headclass = None
+        self.tailclass = None
+        self.tail = None
+        print(self.head)
+        
+        #Initially the singly linked list is empty so it contains no head.
+        
+    def empty(self):
+        Boolean = self.headclass
+        
+        if Boolean == None:
+            
+            print('The singly linked list is empty.')
+            return None
+        
+        else:
+            
+            print('The singly linked list is not empty.')
+            return None
+    
+    def add(self,x):
+        
+        x = Node(x)
+        
+        if self.headclass == None:
+            
+            self.head = x.getData()
+            self.headclass = x
+            self.tail = x.getData()
+            self.headclass = x
+            
+            return self.head
+        
+        else:   
+            
+            self.tailclass.setNext(x)
+
+            PreviousTailNode = self.tailclass
+            self.tailclass = x
+
+            return PreviousTailNode.getNext().getData()
+        
+    #SEARCH finds the first NODE with key k in list L by a simple linear search,  returning a pointer to 
+    #this node.
+    
+    def search(self,k):
+  
+        x = self.headclass
+        
+        if x.getData() == k:
+            
+            return x, x.getData()
+        
+        else:
+            
+            dummyclass = x.getNext()
+            
+            while dummyclass != None:
+                
+                if dummyclass.getData() == k:
+                    
+                    return dummyclass, dummyclass.getData()
+                
+                dummyclass = dummyclass.getNext()
+            
+            Boolean = None
+            
+            return Boolean  
+        
+        
+    #We implement an INSERT function that interests an element x as the new head of the linked list.
+    
+    def insert(self,x):
+        
+        x = Node(x)
+        OldHead = self.headclass
+        
+        x.setNext(OldHead)
+        self.headclass = x
+        
+        return self.headclass.getData(), self.headclass.getNext().getData()
+    
+    #Here we assume that an element with key x is contained in the linked list so we can delete the key
+    #and update the pointers. We delete the first instance of x.
+    
+    def delete(self,k):
+        
+        x = self.headclass
+        
+        if x.getData() == k:
+            
+            newheadclass = x.getNext()
+            
+            if newheadclass == None:
+                
+                self.head = None
+                self.headclass = None 
+                self.tailclass = None
+                self.tail = None
+                
+            else:
+                
+                x.setNext(None)
+                self.headclass = newheadclass               
+            
+                return self.headclass.getData(), self.headclass.getNext().getData() 
+    
+    
+        nextclass = x.getNext()  
+        
+        while nextclass !=None:
+            
+            if nextclass.getData() == k:
+                
+                if nextclass.getNext() !=None:
+                    
+                    nextclass.getPrev().setNext(nextclass.getNext())
+                    
+                    a = nextclass.getPrev().getNext().getData()
+                    
+                    nextclass.setNext(None)
+                    
+                    return a
+                    
+                else:
+                    
+                    self.tailclass = nextclass.getPrev()
+                    self.tailclass.setNext(None)
+                    
+                    return self.tailclass.getData(), self.tailclass.getNext()
+                    
+            nextclass = nextclass.getNext()      
+
+
+# A circularly linked list is similarly implemented by making minor modification to the implementation of the doubly linked list. We skip the details for now.
+
+# ### 10.2.1
+# 
+# See the above code for the implementation of the INSERT function in $O(1)$ time using a singly linked list. First note that if we wish to implement the DELETE function to delete either the head or the tail of the singly linked list, we can easily do so in $O(1)$ time. Deleting the head of the list is easy; simply change the $next$ pointer of the head to NONE, and update the head to the node to which the pointer $head.next$ points. To delete the tail of the singly linked list in $O(1)$ time, modify the implementation of the singly linked list to keep track of the node that precedes the tail of the list. Call the pointer pointing to this node $tailminus$. To remove the tail of the list, simply set the pointer $tailminus.next$ to NONE. We use this strategy below to implement a queue using a singly linked list.
+# 
+# We can, in principle, use the aforementioned strategy to implement the DELETE function in $O(1)$ $time$. We can effevitvely make the node we wish to delete the last node in an appropriate sub-list of our singly linked list; simply keep copies of sub-lists that contain the first node, the first two nodes, the first three nodes etc. Complimentarily, keep copies of the sublists of the remaining elements in the singly linked list. Place these complimentary sub-lists in the the NODE class that corresponds to the last element in the respective sub-list. If the NODE class already contains a complimentayry pair of sub-lists, don't replace the exisiting sub-lists with another pair of complimentary sub-lists; this will ensure that we only remove the first occurence of the node containing the appropriate key. Now simply use the appropriate pair of complimentary sub-lists, remove the last tail node the appropriately chosen sub-list between the top, and make a directed node from the new tail of this sub-list to the head of other sub-list as part of the pair. This gives us the desied singly linked list. The algorithm works in $O(1)$ time, but it may end up using significant memory.
+# 
+# ### 10.2.2
+
+# 
